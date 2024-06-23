c913bf2396f099d5cdf23942e4ee566f6328cfc6
maebo <maeboittiaux@gmail.com>
2024-06-23 20:33:27 +0100

    Add paper holes

--- src/Paper.js
+++ src/Paper.js
@@ -4,6 +4,8 @@ import './Paper.css';
 const Paper = () => {
     return (
         <div className="paper">
+            <div className="hole top-hole"></div>
+            <div className="hole bottom-hole"></div>
         </div>
     );
 };
