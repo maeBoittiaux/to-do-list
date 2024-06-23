5f4cb2a176764f304596ce96ebb7d8ea94b1088b
maebo <maeboittiaux@gmail.com>
2024-06-23 20:54:04 +0100

    Add 'To-Do List' title to the paper component

--- src/Paper.js
+++ src/Paper.js
@@ -4,6 +4,7 @@ import './Paper.css';
 const Paper = () => {
     return (
         <div className="paper">
+            <h1 className="title">To-Do List</h1>
             <div className="hole top-hole"></div>
             <div className="hole bottom-hole"></div>
         </div>
