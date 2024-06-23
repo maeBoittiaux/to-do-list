6df19f340a18be6db5394707917645e72a837d7a
maebo <maeboittiaux@gmail.com>
2024-06-23 17:17:28 +0100

    Update App.js to remove reference to deleted files

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Grade Calculator</h1>
+        <h1>To-Do List</h1>
       </header>
     </div>
   );
