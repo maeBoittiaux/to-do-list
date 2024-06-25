beb70fb6d61bcbba6ed04ce4e3534b48f633db1c
maebo <maeboittiaux@gmail.com>
2024-06-25 09:36:59 +0100

    Add colour theme buttons to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Paper from './Paper';
+import ColourTheme from './ColourTheme';
 
 function App() {
   return (
     <div className="App">
       <Paper />
+      <ColourTheme />
     </div>
   );
 }
--- src/ColourTheme.js
+++ src/ColourTheme.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './ButtonGroup.css';
+import './ColourTheme.css';
 
 const ButtonGroup = () => {
     // State Management
