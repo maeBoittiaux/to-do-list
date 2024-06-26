113088dba9328d6615174fd48195d945a704e320
maebo <maeboittiaux@gmail.com>
2024-06-26 11:35:35 +0100

    Make title edit-able

--- src/Paper.js
+++ src/Paper.js
@@ -3,6 +3,8 @@ import './Paper.css';
 
 const Paper = ({ theme }) => {
     const [showPen, setShowPen] = useState(false);
+    const [isEditing, setIsEditing] = useState(false);
+    const [title, setTitle] = useState("To-Do List");
 
     const handleMouseEnter = () => {
         setShowPen(true);
@@ -11,11 +13,35 @@ const Paper = ({ theme }) => {
         }, 3000);
     };
 
+    const handlePenClick = () => {
+        setIsEditing(true);
+    };
+
+    const handleTitleChange = (event) => {
+        setTitle(event.target.value);
+    };
+
+    const handleTitleBlur = () => {
+        setIsEditing(false);
+    };
+
     return (
         <div className={`paper theme-${theme}`}>
             <div className="title-container" onMouseEnter={handleMouseEnter}>
-                <h1 className="title">To-Do List</h1>
-                {showPen && <span className="pen">&#9998;</span>}
+                {isEditing ? (
+                    <input
+                        className="title-input"
+                        value={title}
+                        onChange={handleTitleChange}
+                        onBlur={handleTitleBlur}
+                        autoFocus
+                    />
+                ) : (
+                    <>
+                        <h1 className="title">{title}</h1>
+                        {showPen && <span className="pen" onClick={handlePenClick}>&#9998;</span>}
+                    </>
+                )}
             </div>
             <div className="hole top-hole"></div>
             <div className="hole bottom-hole"></div>
