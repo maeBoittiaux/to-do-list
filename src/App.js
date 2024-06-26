9ea31406346615fa98283d029505ddcbead19121
maebo <maeboittiaux@gmail.com>
2024-06-26 10:04:02 +0100

    Refactor App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Paper from './Paper';
 import ColourTheme from './ColourTheme';
+import './App.css';
 
 function App() {
+  const [theme, setTheme] = useState(0);
+
+  const handleThemeChange = (themeIndex) => {
+    setTheme(themeIndex);
+  };
+
   return (
     <div className="App">
-      <ColourTheme />
-      <Paper />
+      <ColourTheme theme={theme} onThemeChange={handleThemeChange} />
+      <Paper theme={theme} />
     </div>
   );
 }
